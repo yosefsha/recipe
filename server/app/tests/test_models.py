ce370708417d7fbba5af124601fff5f5f3ffdc07
Yosef Shachnovsky <sh.yosef@gmail.com>
2025-11-13 22:48:02 +0200

    added actions

---
+++ server/app/tests/test_models.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+import django
+
+class SampleTestCase(TestCase):
+    """Sample test case."""
+
+    def test_sample(self):
+        """Test that 1 + 1 equals 2."""
+        self.assertEqual(1 + 1, 2)
+    
+    def test_django_is_installed(self):
+        """Test Django is properly installed."""
+        self.assertTrue(hasattr(django, 'VERSION'))
