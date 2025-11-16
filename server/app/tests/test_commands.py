72997a6cae9b6aea6c4d63726998fcf778b251e6
Yosef Shachnovsky <sh.yosef@gmail.com>
2025-11-16 14:44:43 +0200

    changed to simple test

--- server/app/tests/test_commands.py
+++ server/app/tests/test_commands.py
@@ -6,10 +6,10 @@ from unittest.mock import patch
 from psycopg2 import OperationalError as Psycopg2Error
 from django.core.management import call_command
 from django.db.utils import OperationalError
-from django.test import TestCase
+from django.test import SimpleTestCase, TestCase
 
 @patch('core.management.commands.wait_for_db.Command.check')
-class CommandTests(TestCase):
+class CommandTests(SimpleTestCase):
     """Test commands"""
 
     def test_wait_for_db_ready(self, patched_check):
