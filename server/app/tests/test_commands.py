4b617abdf484bf86bfb51f119b167f88ef52ca40
Yosef Shachnovsky <sh.yosef@gmail.com>
2025-11-16 13:14:56 +0200

    fixed test commands

--- server/app/tests/test_commands.py
+++ server/app/tests/test_commands.py
@@ -6,10 +6,10 @@ from unittest.mock import patch
 from psycopg2 import OperationalError as Psycopg2Error
 from django.core.management import call_command
 from django.db.utils import OperationalError
-from django.test import SimpleTestCase
+from django.test import TestCase
 
 @patch('core.management.commands.wait_for_db.Command.check')
-class CommandTests(SimpleTestCase):
+class CommandTests(TestCase):
     """Test commands"""
     def test_wait_for_db_ready(self, patched_check):
         patched_check.return_value = True
