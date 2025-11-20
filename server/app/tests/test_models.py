1c364c8ca843995bac509aff1ef353c110f9823c
Yosef Shachnovsky <sh.yosef@gmail.com>
2025-11-20 11:33:30 +0200

    added user tests api not implemented yed

--- server/app/tests/test_models.py
+++ server/app/tests/test_models.py
@@ -1,16 +1,21 @@
 from django.test import TestCase
-import django
+from django.contrib.auth import get_user_model
 
-class SampleTestCase(TestCase):
-    """Sample test case."""
+class UserModelCase(TestCase):
+    """Test case user model"""
 
     def test_sample(self):
         """Test that 1 + 1 equals 2."""
         self.assertEqual(1 + 1, 2)
     
     def test_django_is_installed(self):
-        """Test Django is properly installed."""
-        self.assertTrue(hasattr(django, 'VERSION'))
+        """Test create user with email is successful"""
+        email = 'test@example.com'
+        password = 'testpass'
+        user = get_user_model().objects.create_user(email=email, password=password) 
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
     
     def test_yet_another_test(self):
         pass
